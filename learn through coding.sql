d38c433041180745db7b9f40aff9d33ad15e0a48
Shah Mehrab Kabir <mehrabkabir2729@gmail.com>
2025-12-14 12:19:36 +0600

    Update and rename learning through coding.md to learn through coding.sql

--- learn through coding.sql
+++ learn through coding.sql
@@ -1,8 +1,6 @@
-'''
 -- First, list databases to confirm
 SHOW DATABASES;
 
 -- Then delete (choose one method)
 DROP DATABASE franchises;
 -- DROP DATABASE IF EXISTS database_name;  -- Safer: won't error if doesn't exist
-'''
