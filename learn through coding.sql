3dbadceb0e98fbce4c70b1292f5a024926d8f7b5
Shah Mehrab Kabir <mehrabkabir2729@gmail.com>
2025-12-15 12:41:15 +0600

    Update learn through coding.sql

--- learn through coding.sql
+++ learn through coding.sql
@@ -1,6 +1,15 @@
 -- First, list databases to confirm
 SHOW DATABASES;
-
 -- Then delete (choose one method)
 DROP DATABASE franchises;
 -- DROP DATABASE IF EXISTS database_name;  -- Safer: won't error if doesn't exist
+
+-- Get rows where a number is not equal to a value with WHERE col <> n or WHERE col != n
+SELECT franchise, inception_year
+FROM franchises
+WHERE inception_year <> 1996
+
+-- Get the total number of rows SELECT COUNT(*)
+SELECT COUNT(*)
+FROM franchises
+
